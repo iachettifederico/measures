73dab866f05d627934f4c799456b310a47a3e4ca
Federico Iachetti <iachetti.federico@gmail.com>
2022-04-19 19:20:42 -0300

    Can add and substract measures of the same unit

--- lib/measure.rb
+++ lib/measure.rb
@@ -1,4 +1,6 @@
 class Measure
+  attr_reader :amount
+
   @registered_measures = {}
 
   def self.for(amount:, unit:)
@@ -10,12 +12,41 @@ class Measure
     @unit   = unit
   end
 
+  def +(other_measure)
+    assert_same_unit!(other_measure)
+
+    unit * (amount + other_measure.amount)
+  end
+
+  def -(other_measure)
+    assert_same_unit!(other_measure)
+
+    unit * (amount - other_measure.amount)
+  end
+
+  def -@
+    unit * (amount * -1)
+  end
+
   def to_s
-    "#@amount #{@unit.name_for(@amount)}"
+    "#{amount} #{unit.name_for(amount)}"
   end
 
   def inspect
-    "#{self.class}(#{@amount} #{@unit.to_s})"
+    "#{self.class}(#{amount} #{unit.to_s})"
   end
 
+  def unit?(potential_unit)
+    unit == potential_unit
+  end
+
+  private
+
+  attr_reader :unit
+
+  def assert_same_unit!(a_measure)
+    raise CANT_APPLY_OPERATION unless a_measure.unit?(unit)
+  end
+
+  CANT_APPLY_OPERATION="Can't apply operation"
 end
---
+++ spec/suite/addition_and_substraction_spec.rb
@@ -0,0 +1,34 @@
+require "spec_helper"
+
+RSpec.describe "coso" do
+  let(:meter)        { Unit["meter"] }
+  let(:liter)        { Unit["liter"] }
+  let(:one_meter)    { 1*meter }
+  let(:two_meters)   { 2*meter }
+  let(:three_meters) { 3*meter }
+
+  it "adds two measures of the same unit together" do
+    expect(one_meter + two_meters).to eq(three_meters)
+  end
+
+  it "substracts two measures of the same unit together" do
+    expect(one_meter - two_meters).to eq(-one_meter)
+  end
+
+  it "can't add two measures of different units" do
+    expect {
+      one_meter + (1*liter)
+    }.to raise_error(RuntimeError, Measure::CANT_APPLY_OPERATION)
+  end
+
+  it "can't substract two measures of different units" do
+    expect {
+      one_meter - (1*liter)
+    }.to raise_error(RuntimeError, Measure::CANT_APPLY_OPERATION)
+  end
+
+  # suma y resta entre measures de la misma unidad
+  # suma y resta entera entre measures de la misma familia de unidades (1m, 100cm)
+  # suma y resta fraccionaria entre measures de la misma familia de unidades (1m, 1cm)
+  # suma y resta entre measures de distintas familias de unidades (m, lt)
+end
