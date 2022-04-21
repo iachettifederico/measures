e6792fc27a8dce669264049318a98cac326f3924
Federico Iachetti <iachetti.federico@gmail.com>
2022-04-21 12:22:34 -0300

    Reformatting

--- spec/suite/addition_and_substraction_spec.rb
+++ spec/suite/addition_and_substraction_spec.rb
@@ -1,11 +1,11 @@
 require "spec_helper"
 
-RSpec.describe "coso" do
+RSpec.describe "addition and substraction" do
   let(:meter)        { Unit["meter"] }
   let(:liter)        { Unit["liter"] }
-  let(:one_meter)    { 1*meter }
-  let(:two_meters)   { 2*meter }
-  let(:three_meters) { 3*meter }
+  let(:one_meter)    { 1 * meter }
+  let(:two_meters)   { 2 * meter }
+  let(:three_meters) { 3 * meter }
 
   it "adds two measures of the same unit together" do
     expect(one_meter + two_meters).to eq(three_meters)
@@ -17,17 +17,17 @@ RSpec.describe "coso" do
 
   it "can't add two measures of different units" do
     expect {
-      one_meter + (1*liter)
+      one_meter + (1 * liter)
     }.to raise_error(RuntimeError, Measure::CANT_APPLY_OPERATION)
   end
 
   it "can't substract two measures of different units" do
     expect {
-      one_meter - (1*liter)
+      one_meter - (1 * liter)
     }.to raise_error(RuntimeError, Measure::CANT_APPLY_OPERATION)
   end
 
-  # suma y resta entre measures de la misma unidad
+  # DONE suma y resta entre measures de la misma unidad
   # suma y resta entera entre measures de la misma familia de unidades (1m, 100cm)
   # suma y resta fraccionaria entre measures de la misma familia de unidades (1m, 1cm)
   # suma y resta entre measures de distintas familias de unidades (m, lt)
